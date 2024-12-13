1e44e99c09892b4d053a1fa9815678d77c11fb67
AhmedHarb <ahmedharb960@gmail.com>
2024-12-13 16:31:15 +0200

    chore: add MUI packages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
+import { withPigment } from "@pigment-css/nextjs-plugin";
+import { createTheme } from "@mui/material";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withPigment(nextConfig, {
+  theme: createTheme({
+    cssVariables: {
+      colorSchemeSelector: "class",
+    },
+    colorSchemes: { light: true, dark: true },
+    typography: {
+      fontFamily: "var(--font-roboto)",
+    },
+  }),
+  transformLibraries: ["@mui/material"],
+});
