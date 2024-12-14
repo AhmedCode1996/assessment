e8b9495680c552b8452e1c041f49f676704578ef
AhmedHarb <ahmedharb960@gmail.com>
2024-12-14 19:02:16 +0200

    feat: add delete confirmation dialog

---
+++ src/components/DeleteConfirmationDialog.tsx
@@ -0,0 +1,39 @@
+"use client";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Typography,
+  Button,
+} from "@mui/material";
+
+interface DeleteConfirmationDialogProps {
+  open: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+}
+
+export function DeleteConfirmationDialog({
+  open,
+  onClose,
+  onConfirm,
+}: DeleteConfirmationDialogProps) {
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <DialogTitle>Confirm Deletion</DialogTitle>
+      <DialogContent>
+        <Typography>
+          Are you sure you want to delete this image? This action cannot be
+          undone.
+        </Typography>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose}>Cancel</Button>
+        <Button variant="contained" color="error" onClick={onConfirm}>
+          Delete
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
