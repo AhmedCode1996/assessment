f07f1f7261690eb140f3b40495c8e63d92fd0847
AhmedHarb <ahmedharb960@gmail.com>
2024-12-14 18:58:00 +0200

    feat: add index file for custom hooks

---
+++ src/hooks/mutations/index.ts
@@ -0,0 +1,5 @@
+export * from "./useCreateCategory";
+export * from "./useUpdateCategory";
+export * from "./useDeleteCategory";
+export * from "./useDeleteImage";
+export * from "./useUploadImage";
